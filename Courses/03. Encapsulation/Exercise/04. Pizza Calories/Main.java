71dc370e57b74cb488bfb4e775d474eb59f27ebe
Ivan Svirchev <50261200+iSvirchev@users.noreply.github.com>
2019-07-13 13:41:57 +0300

    Implement Task logic

--- Courses/03. Encapsulation/Exercise/04. Pizza Calories/Main.java
+++ Courses/03. Encapsulation/Exercise/04. Pizza Calories/Main.java
@@ -1,17 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+        String pizzaName = scanner.next();
+        int nToppings = scanner.nextInt();
+        scanner.nextLine();
+        scanner.next();
 
-        Pizza pizza = new Pizza("Bulgarian",2);
-        Dough dough = new Dough("Tip500", "Crispy", 100);
-        Topping topping1 = new Topping("Veggies", 50);
-        Topping topping2 = new Topping("Cheese", 50);
-        pizza.addTopping(topping1);
-        pizza.addTopping(topping2);
+        String doughtName = scanner.next();
+        String doughType = scanner.next();
+        double doughWeight = scanner.nextDouble();
+        scanner.nextLine();
+
+        Pizza pizza = new Pizza();
+        Dough dough = new Dough();
+
+        try {
+            pizza = new Pizza(pizzaName, nToppings);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        try {
+            dough = new Dough(doughtName, doughType, doughWeight);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
         pizza.setDough(dough);
 
-        System.out.println(pizza.getName());
-        System.out.println(pizza.getOverallCalories());
+        String end = scanner.nextLine();
 
+        while (!"END".equalsIgnoreCase(end)) {
+            String[] split = end.split("\\s+");
+            String name = split[1];
+            double weight = Double.parseDouble(split[2]);
+
+            try{
+                Topping topping = new Topping(name,weight);
+                pizza.addTopping(topping);
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                return;
+            }
+
+            end = scanner.nextLine();
+        }
+
+        System.out.println(pizza.toString());
     }
 }
