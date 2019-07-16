5c9ab3f6953065a8057ad20435180d6df7de52bb
Ivan Svirchev <50261200+iSvirchev@users.noreply.github.com>
2019-07-16 17:34:19 +0300

    Add files via upload

---
+++ Courses/04. Interfaces and Abstraction/Lab/06. Ferrari/Main.java
@@ -0,0 +1,14 @@
+import ferrari.Ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Ferrari ferrari = new Ferrari(scanner.nextLine());
+
+        System.out.println(ferrari.toString());
+    }
+}
---
+++ Courses/04. Interfaces and Abstraction/Lab/06. Ferrari/ferrari/Ferrari.java
@@ -0,0 +1,38 @@
+package ferrari;
+
+public class Ferrari implements  Car{
+    private String driverName;
+    private String model;
+
+    public Ferrari(String driverName) {
+        this.driverName = driverName;
+        this.model = "488-Spider";
+    }
+
+    @Override
+    public String brakes() {
+        return "Brakes!";
+    }
+
+    @Override
+    public String gas() {
+        return "Zadu6avam sA!";
+    }
+
+    private String getDriverName() {
+        return this.driverName;
+    }
+
+    private String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s/%s/%s",
+                this.getModel(),
+                this.brakes(),
+                this.gas(),
+                this.getDriverName());
+    }
+}
