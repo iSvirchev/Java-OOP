1b04546a8d917986e20c62b26efdaa1885aff59f
Ivan Svirchev <50261200+iSvirchev@users.noreply.github.com>
2019-07-20 14:43:47 +0300

    Add files via upload

---
+++ Courses/05. Polymorphism/Lab/03. Animals/Main.java
@@ -0,0 +1,15 @@
+import animal.Animal;
+import animal.Cat;
+import animal.Dog;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal cat = new Cat("Oscat", "Whiskas");
+        Animal dog = new Dog("Rocky", "Meat");
+
+        System.out.println(cat.explainSelf());
+        System.out.println(dog.explainSelf());
+    }
+}
---
+++ Courses/05. Polymorphism/Lab/03. Animals/animal/Animal.java
@@ -0,0 +1,17 @@
+package animal;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String explainSelf() {
+        return String.format("I am %s and my fabourite food is %s",
+                this.name,
+                this.favouriteFood);
+    }
+}
